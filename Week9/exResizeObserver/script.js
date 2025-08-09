9a4cc86572146835a926cf3ef413ccd9afaccb0b
funnybonesware <sahilsiddiquihere@gmail.com>
2025-08-09 16:15:06 +0530

    vault backup: 2025-08-09 16:15:06

--- Week9/exResizeObserver/script.js
+++ Week9/exResizeObserver/script.js
@@ -5,6 +5,7 @@ const observer = new ResizeObserver(onResize);
 function onResize(entries){
     for(const entry of entries){
         console.clear();
+        console.log("contentRect:", entry);
         console.log("borderBoxSize:", entry.borderBoxSize?.[0]);
         console.log("contentBoxSize:", entry.contentBoxSize?.[0]);
         console.log("devicePixelContentBoxSize:", entry.devicePixelContentBoxSize?.[0]);
